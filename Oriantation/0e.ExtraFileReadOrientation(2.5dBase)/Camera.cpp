07b4e596b86794e7ce15e712151a39620caedc4e
MahiroCreative <127273095+MahiroCreative@users.noreply.github.com>
2023-05-16 11:37:03 +0900

    2.5dベース追加

---
+++ Oriantation/0e.ExtraFileReadOrientation(2.5dBase)/Camera.cpp
@@ -0,0 +1,34 @@
+﻿// 2016 Takeru Yui All Rights Reserved.
+#include "Player.h"
+#include "Camera.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+Camera::Camera()
+{
+	//奥行0.1～1000までをカメラの描画範囲とする
+	SetCameraNearFar(0.1f, 1000.0f);
+
+	pos = VGet(0, 0, 0);
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+Camera::~Camera()
+{
+	// 処理なし.
+}
+
+/// <summary>
+/// 更新
+/// </summary>
+void Camera::Update(const Player& player)
+{
+	VECTOR pos = VAdd(player.GetPos(), VGet(0, 0, -10.0f));
+	VECTOR lookPos = VAdd(player.GetPos(), VGet(0, 1.0f, 0));
+
+	// カメラに位置を反映.
+	SetCameraPositionAndTarget_UpVecY(pos, lookPos);
+}
---
+++ Oriantation/0e.ExtraFileReadOrientation(2.5dBase)/main.cpp
@@ -0,0 +1,65 @@
+﻿// 2023 Takeru Yui All Rights Reserved.
+#include "DxLib.h"
+#include "Player.h"
+#include "Camera.h"
+
+/// <summary>
+/// メイン関数
+/// </summary>
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) 
+{
+	ChangeWindowMode(TRUE);
+
+	// ＤＸライブラリ初期化処理
+	if (DxLib_Init() == -1)		
+	{
+		return -1;	// エラーが起きたら直ちに終了
+	}
+
+	// 画面モードのセット
+	SetGraphMode(1600, 900, 16);
+
+	// カメラを生成.
+	Camera* camera = new Camera();
+
+	// プレイヤーを生成.
+	Player* player = new Player();
+
+	// エスケープキーが押されるかウインドウが閉じられるまでループ
+	LONGLONG frameTime = 0;
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+		frameTime = GetNowHiPerformanceCount();
+		// プレイヤー制御.
+		player->Update();
+		camera->Update(*player);
+
+		// 画面を初期化する
+		ClearDrawScreen();
+
+		// 描画
+		player->Draw();
+
+		// デバッグ描画
+		float lineSize = 10.0f;
+		DrawLine3D(VGet(-lineSize, 0, 0), VGet(lineSize, 0, 0), GetColor(255, 0, 0));
+		DrawLine3D(VGet(0, -lineSize, 0), VGet(0, lineSize, 0), GetColor(0, 255, 0));
+		DrawLine3D(VGet(0, 0, -lineSize), VGet(0, 0, lineSize), GetColor(0, 0, 255));
+
+		// 裏画面の内容を表画面に反映させる
+		ScreenFlip();
+
+		// 雑に60fps固定
+		while ((GetNowHiPerformanceCount() - frameTime) < 16667) {}
+	}
+
+	// 後始末
+	delete(player);
+	delete(camera);
+
+	// ＤＸライブラリの後始末
+	DxLib_End();
+
+	// ソフトの終了
+	return 0;
+}
