fbf7bb27ef43a521abca9cde42301f5ce073e6b0
MahiroCreative <127273095+MahiroCreative@users.noreply.github.com>
2023-06-15 11:40:18 +0900

    ベクトル演習をちょっと修正

--- MyDxLibGame2D/10.MyDxLibGame2D(Vector)/Player.cpp
+++ MyDxLibGame2D/10.MyDxLibGame2D(Vector)/Player.cpp
@@ -9,14 +9,16 @@ void InitPlayer(Player& player)
 	player.obj.graph = LoadGraph("data/texture/player.png");
 	player.obj.pos.x = 288;
 	player.obj.pos.y = 400;
-	player.obj.dir = VGet(0,0,0);
-	player.speed = 3.45f;
+	player.speed = 10;
 
 	// プレイヤーの画像のサイズを得る
 	int w, h;
 	GetGraphSize(player.obj.graph, &w, &h);
 	player.obj.w = (float)w;
 	player.obj.h = (float)h;
+
+	player.obj.dir.x = 0;
+	player.obj.dir.y = 0;
 }
 
 //------------------------------//
@@ -25,33 +27,22 @@ void InitPlayer(Player& player)
 void UpdatePlayer(Player& player)
 {
 	// 矢印キーを押していたらプレイヤーを移動させる
-	float x = 0;
-	float y = 0;
 	if (CheckHitKey(KEY_INPUT_UP) == 1)
 	{
-		y = -1;
+		player.obj.pos.y -= player.speed;
 	}
 	if (CheckHitKey(KEY_INPUT_DOWN) == 1)
 	{
-		y = 1;
+		player.obj.pos.y += player.speed;
 	}
 	if (CheckHitKey(KEY_INPUT_LEFT) == 1)
 	{
-		x = -1;
+		player.obj.pos.x -= player.speed;
 	}
 	if (CheckHitKey(KEY_INPUT_RIGHT) == 1)
 	{
-		x = 1;
+		player.obj.pos.x += player.speed;
 	}
-	player.obj.dir = VGet(x, y, 0);
-
-	if (VSquareSize(player.obj.dir) != 0)
-	{
-		player.obj.dir = VNorm(player.obj.dir);
-	}
-
-	VECTOR velocity = VScale(player.obj.dir, player.speed);
-	player.obj.pos = VAdd(player.obj.pos, velocity);
 
 	// プレイヤーが画面端からはみ出そうになっていたら画面内の座標に戻してあげる
 	if (player.obj.pos.x < 0)
--- MyDxLibGame2D/10.MyDxLibGame2D(Vector)/main.cpp
+++ MyDxLibGame2D/10.MyDxLibGame2D(Vector)/main.cpp
@@ -13,7 +13,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	// 画面モードの設定
 	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, 16);		// 解像度を640*480、colorを16bitに設定.
 	ChangeWindowMode(TRUE);			// ウインドウモードに.
-	
+
 	// ＤＸライブラリ初期化処理
 	if (DxLib_Init() == -1)
 	{
@@ -30,6 +30,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	// エネミー初期化.
 	Enemy enemy;
 	InitEnemy(enemy);
+
+	//////////////////////
+	// ベクトル計算のテスト.
+	VECTOR v1, v2, v3;
+	v1.x = 0;
+	v1.y = 1;
+	v2.x = 2;
+	v2.y = 3;
+	v3 = VAdd(v1, v2);
+	printfDx("x:%f y:%f\n", v3.x, v3.y);
+	v3 = VSub(v1, v2);
+	printfDx("x:%f y:%f\n", v3.x, v3.y);
+	printfDx("len:%f\n", VSize(v3));
+	v3 = VNorm(v3);
+	printfDx("x:%f y:%f\n", v3.x, v3.y);
+	//////////////////////
 
 	// ゲームループ.
 	while (1)
