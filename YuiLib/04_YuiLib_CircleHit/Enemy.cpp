91bcb7fb41002f0df22678981f9ee157aafad3bf
ohara <t.yui@vikingmaxx.com>
2024-05-23 10:05:46 +0900

    円同士の当たり判定を追加

---
+++ YuiLib/04_YuiLib_CircleHit/Enemy.cpp
@@ -0,0 +1,69 @@
+﻿// 2024 Takeru Yui All Rights Reserved.
+#include "DxLib.h"
+#include "Screen.h"
+#include "YuiLib.h"
+#include "Enemy.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+Enemy::Enemy()
+	: Collidable		(YuiLib::Collidable::Tag::Enemy)
+	, graphicHandle		(-1)
+{
+	// 処理なし
+}
+
+/// <summary>
+/// 初期化
+/// </summary>
+void Enemy::Initialize(YuiLib::Physics* physics)
+{
+	physics->Entry(this);	// 物理情報に自身を登録
+
+	// 敵のグラフィックをメモリにロード＆表示座標を初期化
+	graphicHandle = LoadGraph("data/texture/enemy.png");
+
+	// 物理挙動の初期化
+	rigidbody.Initialize();
+	rigidbody.SetPos(VGet(200, 200, 0));
+}
+
+/// <summary>
+/// 終了
+/// </summary>
+void Enemy::Finalize(YuiLib::Physics* physics)
+{
+	physics->Exit(this);	// 物理情報登録解除
+}
+
+/// <summary>
+/// 更新
+/// </summary>
+void Enemy::Update()
+{
+	// TODO: 移動
+}
+
+/// <summary>
+/// 描画
+/// </summary>
+void Enemy::Draw()
+{
+	DrawRotaGraph(
+		static_cast<int>(rigidbody.GetPos().x),
+		static_cast<int>(rigidbody.GetPos().y),
+		1.0f,
+		0,
+		graphicHandle,
+		TRUE);
+}
+
+/// <summary>
+/// 衝突したとき
+/// </summary>
+void Enemy::OnCollide()
+{
+	printfDx("エネミー当たった！");
+}
+
---
+++ YuiLib/04_YuiLib_CircleHit/Enemy.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+class Physics;
+class Collidable;
+
+/// <summary>
+/// 敵クラス。物理・衝突判定をするため、Collidableを継承
+/// </summary>
+class Enemy : public YuiLib::Collidable
+{
+public:
+	Enemy();
+	void Initialize(YuiLib::Physics* physics);
+	void Finalize(YuiLib::Physics* physics);
+	void Update();
+	void Draw();
+
+	// 衝突したとき
+	void OnCollide() override;
+
+private:
+	int		graphicHandle;	// 画像グラフィックハンドル
+};
