cd9f42be449ebfe4b3f33b67a83a24ac2ec63bc6
MahiroCreative <t.yui@vikingmaxx.com>
2024-11-12 12:12:26 +0900

    カメラが反転し損ねていたのを修正

--- 2dActionSample/01_1.2dActionSample(ScrollCameraPosition)/Camera.cpp
+++ 2dActionSample/01_1.2dActionSample(ScrollCameraPosition)/Camera.cpp
@@ -12,20 +12,20 @@ void UpdateCamera(Camera& camera)
 	// カメラが右に移動（表示物は全て左に移動）
 	//camera.pos.x += 0.1f;	// そのままだと反対に表示物が動く
 
-	// カメラが下に移動（表示物は全て上に移動）
+	// カメラが下に移動（表示物は全て下に移動）
 	//camera.pos.y += 0.1f;	// そのままだと上に表示物が動く
 
 	//// プレイヤーの位置とカメラの位置を同じにする ////
-	camera.pos = ? ;
+	camera.pos = ???;
 
 	// ↑ベクトルや計算を使って、「カメラのポジションを動かす」
 	// という感覚を保ちたいので、Draw側に足しているcamera.posをいじる
-	// Draw側に足しているcamera.pos.xは反転させる
+	// Draw側に足しているcamera.pos.x/yは反転させる
 	camera.drawOffset.x = camera.pos.x * -1;
-	camera.drawOffset.y = camera.pos.y;
+	camera.drawOffset.y = camera.pos.y * -1;
 
 	// その時、画面の中央にプレイヤーが来るようにする
 	// (camera.posが画面の中央になるようにする)
 	camera.drawOffset.x = ? ? ? ;
-	camera.drawOffset.y = ???;
+	camera.drawOffset.y = ? ? ? ;
 }
