1344001d2ef03d2b209ea831b64b79b2eedc7637
MahiroCreative <t.yui@vikingmaxx.com>
2024-11-14 11:42:50 +0900

    答え追加

---
+++ 2dActionSample/01_2.2dActionSample(ControlCameraPosition)_Answer/Camera.cpp
@@ -0,0 +1,56 @@
+﻿#include "DxLib.h"
+#include "Camera.h"
+#include "Player.h"
+#include "Screen.h"
+
+// カメラ範囲
+const float CameraScopeRangeW = 400.0f;
+const float CameraScopeRangeH = 300.0f;
+
+void InitCamera(Camera& camera)
+{
+	camera.pos = VGet(0, 0, 0);
+}
+
+void UpdateCamera(Camera& camera, const Player& player)
+{
+	// プレイヤーの位置が、カメラの中央から一定以上離れたら
+	// カメラのポジションをその範囲内に留める
+	if (player.pos.x > camera.pos.x + (CameraScopeRangeW * 0.5f))
+	{
+		camera.pos.x = player.pos.x - (CameraScopeRangeW * 0.5f);
+	}
+	else if (player.pos.x < camera.pos.x - (CameraScopeRangeW * 0.5f))
+	{
+		camera.pos.x = player.pos.x + (CameraScopeRangeW * 0.5f);
+	}
+	if (player.pos.y > camera.pos.y + (CameraScopeRangeH * 0.5f))
+	{
+		camera.pos.y = player.pos.y - (CameraScopeRangeH * 0.5f);
+	}
+	else if (player.pos.y < camera.pos.y - (CameraScopeRangeH * 0.5f))
+	{
+		camera.pos.y = player.pos.y + (CameraScopeRangeH * 0.5f);
+	}
+
+	printfDx("camera.pos:(%d,%d) player.pos:(%d,%d)\n",
+		(int)camera.pos.x,
+		(int)camera.pos.y,
+		(int)player.pos.x,
+		(int)player.pos.y);
+
+	/////////////////////////////////////////////////////////////////////////////
+	// ↓drawOffset補正
+	// （カメラを動かしている感覚で他の描画物のポジションをいじれるようにする計算
+	/////////////////////////////////////////////////////////////////////////////
+	// ↑ベクトルや計算を使って、「カメラのポジションを動かす」
+	// という感覚を保ちたいので、Draw側に足しているcamera.posをいじる
+	// Draw側に足しているcamera.pos.x/yは反転させる
+	camera.drawOffset.x = camera.pos.x * -1;
+	camera.drawOffset.y = camera.pos.y * -1;
+
+	// その時、画面の中央にプレイヤーが来るようにする
+	// (camera.posが画面の中央になるようにする)
+	camera.drawOffset.x = camera.drawOffset.x + (ScreenWidth * 0.5f);
+	camera.drawOffset.y = camera.drawOffset.y + (ScreenHeight * 0.5f);
+}
---
+++ 2dActionSample/01_2.2dActionSample(ControlCameraPosition)_Answer/main.cpp
@@ -0,0 +1,70 @@
+﻿//-----------------------------------------------------------------------------
+// @brief  2Dアクションサンプルプログラム.
+// 2024 Takeru Yui All Rights Reserved.
+//-----------------------------------------------------------------------------
+
+#include "DxLib.h"
+#include "Screen.h"
+#include "Map.h"
+#include "Player.h"
+#include "Camera.h"
+
+/// <summary>
+/// Main
+/// </summary>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	SetGraphMode(ScreenWidth, ScreenHeight, 32);		// 解像度をSCREEN_W*SCREEN_H、colorを32bitに設定.
+
+	ChangeWindowMode(TRUE);
+
+	// ＤＸライブラリの初期化
+	if (DxLib_Init() == -1) return -1;
+
+	// 描画先を裏画面にセット
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	// カメラ初期化
+	Camera camera;
+	InitCamera(camera);
+
+	// プレイヤー初期化
+	Player player;
+	InitPlayer(player);
+
+	// マップ初期化
+	Map map;
+	InitMap(map);
+
+	// メインループ開始、ＥＳＣキーで外に出る
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+		auto prevTime = GetNowHiPerformanceCount();	// 処理が始まる前の時間
+		
+		ClearDrawScreen();// 画面のクリア
+
+		// 更新
+		UpdatePlayer(player, map);
+		UpdateCamera(camera, player);
+
+		// 描画
+		DrawPlayer(player, camera);
+		DrawMap(map, camera);
+		
+		ScreenFlip();	// 画面の更新
+
+		// 雑なfps固定処理
+		// 差を求めて、1回の画面更新が1/60秒になるようにwhileループ回して待つ
+		auto afterTime = GetNowHiPerformanceCount(); // 処理が終わった後の時間
+		while (afterTime - prevTime < 16667)
+		{
+			afterTime = GetNowHiPerformanceCount();
+		}
+	}
+
+	// ＤＸライブラリの後始末
+	DxLib_End();
+
+	// 終了
+	return 0;
+}
