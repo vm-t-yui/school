020ce2f80e382d34f40870ea73f134610a906fbb
MahiroCreative <t.yui@vikingmaxx.com>
2024-11-26 12:58:39 +0900

    ハイスピード版追加中

---
+++ 2dActionSample/01.2dActionSample(HighSpeed)_Answer/Map.h
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------------
+// 2024 Takeru Yui All Rights Reserved.
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+//-----------------------------------------//
+// 定数
+//-----------------------------------------//
+const int MapChipSize = 32;                           // 一つのチップのサイズ
+const int MapWidth = ScreenWidth / MapChipSize;    // マップの横幅
+const int MapHeight = ScreenHeight / MapChipSize;   // マップの縦幅
+
+/// <summary>
+/// マップチップ構造体
+/// </summary>
+struct MapChip
+{
+	VECTOR pos;	// 座標
+	float w, h;	// 幅、高さ
+	int chipKind;
+};
+
+/// <summary>
+/// マップ構造体
+/// </summary>
+struct Map
+{
+	MapChip mapChips[MapWidth][MapHeight];
+};
+
+//-----------------------------------------//
+// プロトタイプ宣言
+//-----------------------------------------//
+void InitMap(Map& map);
+void DrawMap(const Map& map);
