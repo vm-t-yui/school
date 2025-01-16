1ffab882508fc884faf76ac8d19568104b7e8515
MahiroCreative <t.yui@vikingmaxx.com>
2025-01-16 12:35:13 +0900

    stdcontainerサンプル追加

---
+++ Oriantation/08.ObjectOrientation2(StdContainer)/Shot.cpp
@@ -0,0 +1,82 @@
+﻿//-----------------------------------------------------------------------------
+// @brief  ショット処理.
+//-----------------------------------------------------------------------------
+#include "Common.h"
+#include "DxLib.h"
+#include "Shot.h"
+#include "Enemy.h"
+
+//----------------------------//
+// ショット関数群.
+//----------------------------//
+// 初期化.
+void Shot::Init()
+{
+	// ショットのグラフィックをメモリにロード.
+	Graph = LoadGraph("data/texture/SuperShot.png");
+
+	// 弾が画面上に存在しているか保持する変数に『存在していない』を意味するfalseを代入しておく
+	isVisible = false;
+
+	// 弾のグラフィックのサイズをえる
+	GetGraphSize(Graph, &W, &H);
+}
+
+// アップデート.
+void Shot::Update(Enemy& enemy)
+{
+	// 自機の弾iの移動ルーチン( 存在状態を保持している変数の内容がtrue(存在する)の場合のみ行う )
+	if (isVisible == true)
+	{
+		// 弾iを移動させる
+		Y -= SHOT_SPEED;
+
+		// 画面外に出てしまった場合は存在状態を保持している変数にfalse(存在しない)を代入する
+		if (Y < 0 - H)
+		{
+			isVisible = false;
+		}
+	}
+
+	// 弾のあたり判定.
+	// 弾iが存在している場合のみ次の処理に映る
+	if (isVisible == 1 && enemy.GetLife() > 0)
+	{
+		// エネミーとの当たり判定
+		if (((X > enemy.GetX() && X < enemy.GetX() + enemy.GetW()) ||
+			(enemy.GetX() > X && enemy.GetX() < X + W)) &&
+			((Y > enemy.GetY() && Y < enemy.GetY() + enemy.GetH()) ||
+				(enemy.GetY() > Y && enemy.GetY() < Y + H)))
+		{
+			// 接触している場合は当たった弾の存在を消す
+			isVisible = 0;
+
+			enemy.OnDamage();
+		}
+	}
+}
+// 描画.
+void Shot::Draw()
+{
+	if (isVisible == true)
+	{
+		// 画面に弾iを描画する
+		DrawGraph(X, Y, Graph, TRUE);
+	}
+}
+
+/// <summary>
+/// 弾が撃たれたとき
+/// </summary>
+void Shot::OnShoot(int shooterX, int shooterY, int shooterW, int shooterH)
+{
+	// HACK:引数を(int startX, int startY)とし、(shooterW - W) / 2 + shooterXの計算を外でやるほうがスマート
+
+	// 弾iの位置をセット、位置は弾を撃ったやつの中心にする
+	X = (shooterW - W) / 2 + shooterX;
+	Y = (shooterH - H) / 2 + shooterY;
+
+	// 弾iは現時点を持って存在するので、存在状態を保持する変数にtrueを代入する
+	isVisible = true;
+}
+
---
+++ Oriantation/08.ObjectOrientation2(StdContainer)/main.cpp
@@ -0,0 +1,121 @@
+﻿//-----------------------------------------------------------------------------
+// @brief  メイン処理.
+// 2016 Takeru Yui All Rights Reserved.
+//-----------------------------------------------------------------------------
+#include "DxLib.h"
+#include "Common.h"
+#include "Player.h"
+#include "Shot.h"
+#include "Enemy.h"
+//#include <vector>
+#include <list>
+
+//-----------------------------------------------------------------------------
+// @brief  メイン関数.
+//-----------------------------------------------------------------------------
+
+//----------------------------//
+// グローバル変数.
+//----------------------------//
+Player player;
+Enemy enemy;
+//Shot shot[SHOT];
+std::list<Shot> shot(SHOT);
+
+//----------------------------//
+// WinMain関数.
+//----------------------------//
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	// 画面モードの設定
+	SetGraphMode(SCREEN_W, SCREEN_H, 16);		// 解像度をSCREEN_W*SCREEN_H、colorを16bitに設定.
+	ChangeWindowMode(TRUE);			// ウインドウモードに.
+	
+	// ＤＸライブラリ初期化処理
+	if (DxLib_Init() == -1)
+	{
+		return -1;
+	}
+
+	// 背景のスクロールをするためにテクスチャモードをラッピングに修正.
+	SetTextureAddressMode(DX_TEXADDRESS_WRAP);
+
+	// グラフィックの描画先を裏画面にセット
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	// 各初期化を呼ぶ.
+	player.Init();
+	enemy.Init();
+
+	// vector型だったり、中身が増減しないならそのまま使える
+	//for (int i = 0; i < SHOT; i++)
+	//{
+	//	shot[i].Init();
+	//}
+	// 配列と同じ添え字アクセスが出来ないやつ
+	// 中身が増減するやつはこっち
+	// 範囲for分
+	for (auto& item : shot)
+	{
+		item.Init();
+	}
+
+	// ゲームループ.
+	while (1)
+	{
+		// 画面を初期化(真っ黒にする)
+		ClearDrawScreen();
+
+		// 各アップデート関数を呼ぶ.
+		player.Update(shot);
+		enemy.Update();
+
+		// Xキーを押したらショットの中身が増える
+		if (CheckHitKey(KEY_INPUT_X) == 1)
+		{
+			// !この方法だと、新しく作った初期化関数Initが呼べない！
+			//shot.emplace_back();	// 後ろに追加
+
+			// !この方法だとInitは呼べるものの、
+			// 無駄なローカル変数が生まれて破棄される！
+			Shot newShot;
+			newShot.Init();
+			shot.emplace_back(newShot);
+
+			// ほんとはポインタにしておくと無駄が少ない
+		}
+		printfDx("弾の総数%d\n", shot.size() );
+		for (auto& item : shot)
+		{
+			item.Update(enemy);
+		}
+
+		// 各描画関数を呼ぶ.
+		for (auto& item : shot)
+		{
+			item.Draw();
+		}
+		player.Draw();
+		enemy.Draw();
+
+		// 裏画面の内容を表画面にコピーする（描画の確定）.
+		ScreenFlip();
+
+		// Windows 特有の面倒な処理をＤＸライブラリにやらせる
+		// マイナスの値（エラー値）が返ってきたらループを抜ける
+		if (ProcessMessage() < 0)
+		{
+			break;
+		}
+		// もしＥＳＣキーが押されていたらループから抜ける
+		else if (CheckHitKey(KEY_INPUT_ESCAPE))
+		{
+			break;
+		}
+	}
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;					// ソフトの終了
+}
