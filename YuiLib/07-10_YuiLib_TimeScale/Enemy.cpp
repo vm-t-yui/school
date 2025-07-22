ef75736599037fb31a4db3797e6802ba9c3cebc6
Takeru Yui <49709064+vm-t-yui@users.noreply.github.com>
2025-07-22 12:08:27 +0900

    timeScale再実装

---
+++ YuiLib/07-10_YuiLib_TimeScale/Enemy.cpp
@@ -0,0 +1,93 @@
+﻿// 2024 Takeru Yui All Rights Reserved.
+#include "DxLib.h"
+#include <string>
+#include "Screen.h"
+#include "YuiLib.h"
+#include "WorldSprite.h"
+#include "Enemy.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+Enemy::Enemy()
+	: Collidable		(Collidable::Priority::Low, GameObjectTag::Enemy, YuiLib::ColliderData::Kind::Circle2D, false)
+	, graphicHandle		(-1)
+	, worldSprite		(nullptr)
+{
+	auto circleColliderData = dynamic_cast<YuiLib::ColliderDataCircle2D*>(colliderData);
+	circleColliderData->radius = 0.5f;
+	AddThroughTag(GameObjectTag::StepGround);
+}
+
+/// <summary>
+/// 初期化
+/// </summary>
+void Enemy::Initialize(YuiLib::Physics* physics)
+{
+	Collidable::Initialize(physics);
+
+	// 敵のグラフィックをメモリにロード＆表示座標を初期化
+	graphicHandle = LoadGraph("data/texture/enemy.png");
+	worldSprite = new YuiLib::WorldSprite();
+	worldSprite->Initialize(graphicHandle, 64, 0);
+
+	// 物理挙動の初期化
+	rigidbody.Initialize();
+	rigidbody.SetPos(VGet(-2.0f, 4.0f, 0));
+}
+
+/// <summary>
+/// 終了
+/// </summary>
+void Enemy::Finalize(YuiLib::Physics* physics)
+{
+	delete worldSprite;
+	worldSprite = nullptr;
+	Collidable::Finalize(physics);
+}
+
+/// <summary>
+/// 更新
+/// </summary>
+void Enemy::Update()
+{
+	// どこにも移動しない
+	rigidbody.SetVelocity(VGet(0, 0, 0));
+}
+
+/// <summary>
+/// 描画
+/// </summary>
+void Enemy::Draw()
+{
+	// エネミーの顔＝1.0mのつもりで設定
+	worldSprite->SetTransform(rigidbody.GetPos(), 1.0f);
+	worldSprite->Draw();
+}
+
+/// <summary>
+/// 衝突したとき
+/// </summary>
+void Enemy::OnCollide(const Collidable& colider)
+{
+	std::string message = "敵が";
+	if (colider.GetTag() == GameObjectTag::Player)
+	{
+		message += "プレイヤー";
+	}
+	else if (colider.GetTag() == GameObjectTag::Enemy)
+	{
+		message += "敵";
+	}
+	else if (colider.GetTag() == GameObjectTag::SystemWall)
+	{
+		message += "システム壁";
+	}
+	else if (colider.GetTag() == GameObjectTag::StepGround)
+	{
+		message += "足場";
+	}
+	message += "と当たった！\n";
+	//printfDx(message.c_str());
+}
+
---
+++ YuiLib/07-10_YuiLib_TimeScale/SystemWall.cpp
@@ -0,0 +1,67 @@
+﻿// 2024 Takeru Yui All Rights Reserved.
+#include "DxLib.h"
+#include <string>
+#include "Screen.h"
+#include "YuiLib.h"
+#include "SystemWall.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+SystemWall::SystemWall(const VECTOR& start, const VECTOR& end)
+	: Collidable		(Collidable::Priority::Static, GameObjectTag::SystemWall, YuiLib::ColliderData::Kind::Line2D, false)
+{
+	auto lineColliderData = dynamic_cast<YuiLib::ColliderDataLine2D*>(colliderData);
+
+	// 壁の位置を作る
+	lineColliderData->startPoint = start;
+	lineColliderData->endPoint	 = end;
+}
+
+/// <summary>
+/// 初期化
+/// </summary>
+void SystemWall::Initialize(YuiLib::Physics* physics)
+{
+	Collidable::Initialize(physics);
+
+	// 物理挙動の初期化
+	rigidbody.Initialize();
+
+	// 動かないので物理的な位置は無視。仮で始点にする
+	auto lineColliderData = dynamic_cast<YuiLib::ColliderDataLine2D*>(colliderData);
+	rigidbody.SetPos(lineColliderData->startPoint);
+}
+
+
+/// <summary>
+/// 更新
+/// </summary>
+void SystemWall::Update()
+{
+	// どこにも移動しない
+	rigidbody.SetVelocity(VGet(0, 0, 0));
+}
+
+/// <summary>
+/// 衝突したとき
+/// </summary>
+void SystemWall::OnCollide(const Collidable& colider)
+{
+	std::string message = "システム壁が";
+	if (colider.GetTag() == GameObjectTag::Player)
+	{
+		message += "プレイヤー";
+	}
+	else if (colider.GetTag() == GameObjectTag::Enemy)
+	{
+		message += "敵";
+	}
+	else
+	{
+		// 壁同士は当たらないので無視
+	}
+	message += "と当たった！\n";
+	//printfDx(message.c_str());
+}
+
