e564f85001b71d88e432b7e40462577ef2a6ad19
MahiroCreative <49709064+vm-t-yui@users.noreply.github.com>
2025-11-06 12:02:07 +0900

    アニメーション切り替えの答え

---
+++ MyDxLibGame2D/12.MyDxLibGame2D(3D_Animation)_Answer/Enemy.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+/// <summary>
+/// エネミークラス
+/// </summary>
+class Enemy
+{
+public:
+	// 敵の状態を表す列挙体
+	enum class State : int
+	{
+		Normal,		// 通常
+		Damage		// ダメージ
+	};
+
+	VECTOR	pos;
+	VECTOR	dir;	// エネミーの向き
+	int		graphNormal;
+	int		graphDamage;
+	int		graph;
+	int		w;
+	int		h;
+	bool	isRightMove;
+	State	state;
+	int		damageCount;
+
+	int		modelHandle;
+	int		currentAnimAttachIndex;		// アタッチしたアニメーションのインデックス
+	float	currentAnimTotalTime;		// 再生中のアニメーションのトータル時間
+	float	currentAnimTimeCount;		// 再生中のアニメーションの現在時間
+	float	currentAnimSpeed;			// 再生中のアニメーションスピード
+	bool	currentAnimIsLoop;			// ループするかどうか
+
+	constexpr static float		Speed		= 3.0f;
+	constexpr static float		HitSize		= 30;	// エネミーの当たり判定サイズ
+	constexpr static int		DamageTime	= 30;	// エネミーのダメージ顔になっている時間
+	inline const static VECTOR	FirstPos	= VGet(0, 50, 0);
+
+	// モデルの表示スケールと位置補正
+	inline const static VECTOR	ModelScale	= VGet(0.003f, 0.003f, 0.003f);
+	inline const static VECTOR	ModelFixPos	= VGet(0.0f, -0.25f, 0.0f);
+
+	constexpr static int	IdleAnimationIndex		= 2;	// 待機アニメーションインデックス
+	constexpr static float	IdleAnimationSpeed		= 0.3f;	// 待機アニメーションスピード
+	constexpr static int	DamageAnimationIndex	= 4;	// ダメージアニメーションインデックス
+	constexpr static float	DamageAnimationSpeed	= 0.8f;	// ダメージアニメーションスピード
+
+	void Initialize();
+	void Update();
+	void Draw() const;
+
+	void ChangeAnimation(int animationIndex, float speed, bool isLoop);	// アニメーションの切り替え
+};
---
+++ MyDxLibGame2D/12.MyDxLibGame2D(3D_Animation)_Answer/Player.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+class Input;	// プロトタイプ宣言
+class Shot;		// プロトタイプ宣言
+
+/// <summary>
+/// プレイヤークラス
+/// </summary>
+class Player
+{
+public:
+	VECTOR	pos;
+	VECTOR	dir;
+	int		graph;
+	int		w, h;
+
+	int		modelHandle;		// 3Dモデルのハンドル（graphの3D版）
+
+	// constexpr定数は後ろにstatic(C++17以降)
+	constexpr static float	Speed	= 3.0f;
+	constexpr static float	HitSize = 30;	// 当たり判定サイズ
+
+	// constは頭にinline,後ろにstatic(C++17以降)
+	inline const static	VECTOR	FirstPos = VGet(Graphics::ScreenW * 0.5f, Graphics::ScreenH - 80.0f, 0);
+
+	// モデルの表示スケールと位置補正
+	inline const static VECTOR	ModelScale	= VGet(0.002f, 0.002f, 0.002f);
+	inline const static VECTOR	ModelFixPos	= VGet(0.0f, -0.3f, 0.0f);
+
+	void Initialize();
+	void Update(const Input& input, std::vector<Shot>& shotArray);
+	void Draw() const;
+};
