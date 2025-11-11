c9e825073b662bea2745e480d4314555071b1328
MahiroCreative <49709064+vm-t-yui@users.noreply.github.com>
2025-11-11 11:33:11 +0900

    Mixamo演習サンプル追加

---
+++ MyDxLibGame2D/13.MyDxLibGame2D(3D_MixamoAnimation)/Player.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+class Input;	// プロトタイプ宣言
+class Shot;		// プロトタイプ宣言
+
+/// <summary>
+/// プレイヤークラス
+/// </summary>
+class Player
+{
+public:
+	VECTOR	pos;
+	VECTOR	dir;
+
+	int		modelHandle;		// 3Dモデルのハンドル（graphの3D版）
+
+	// constexpr定数は後ろにstatic(C++17以降)
+	constexpr static float	Speed	= 8.0f;
+	constexpr static float	HitSize = 30;	// 当たり判定サイズ
+
+	// constは頭にinline,後ろにstatic(C++17以降)
+	inline const static	VECTOR	FirstPos = VGet(100.0f, Graphics::ScreenH * 0.5f, 0);
+
+	// モデルの表示スケールと位置補正
+	inline const static VECTOR	ModelScale	= VGet(0.002f, 0.002f, 0.002f);
+	inline const static VECTOR	ModelFixPos	= VGet(0.0f, -0.3f, 0.0f);
+
+	void Initialize();
+	void Update(const Input& input, std::vector<Shot>& shotArray);
+	void Draw() const;
+};
